5760da209f5afa8edda7c760f03ada527fc539f7
Sujal Kumar <sujal309k@gmail.com>
2025-09-19 20:26:45 +0530

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['res.cloudinary.com','www.freeiconspng.com','images.unsplash.com', 'placehold.co', 'plus.unsplash.com','randomuser.me'],
+        domains: ['res.cloudinary.com','www.freeiconspng.com','images.unsplash.com', 'placehold.co', 'plus.unsplash.com','randomuser.me','avatars.dicebear.com','lh3.googleusercontent.com','firebasestorage.googleapis.com','img.freepik.com','cloudflare-ipfs.com','media.istockphoto.com','t3.ftcdn.net','cdn-icons-png.flaticon.com','img.icons8.com','mdbcdn.b-cdn.net','img.freepik.com','png.pngtree.com','png.pngtree.com','png.pngtree.com','png.pngtree.com','png.pngtree.com','png.pngtree.com','png.pngtree.com'],
     }
 };
 
